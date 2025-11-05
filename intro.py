cc3d4dd2ea518c28c1a234aaf2b2b973276187d3
palaciossusang-debug <palaciossusang@gmail.com>
2025-11-05 13:07:47 -0500

    Add script to query OpenAI API for tennis results
    This script prompts OpenAI's API to get the latest US Open Tennis winner.

---
+++ intro.py
@@ -0,0 +1,27 @@
+import openai
+import getpass
+
+openai.api_key = getpass.getpass("Ingresa tu API Key de OpenAI : ")
+model = "gpt-3.5-turbo"#"gpt-5" #"gpt-5"#"gpt-3.5-turbo" #gpt-4"
+
+# Define el mensaje de entrada para el chat
+prompt = "Quien fue el ultimo ganador del US Open de Tenis"
+message_input = {
+    'messages': [
+        {'role': 'system', 'content': 'Eres un asistente virtual'},
+        {'role': 'user', 'content': prompt}
+    ]
+}
+
+# Realiza una solicitud a la API de OpenAI
+response = openai.ChatCompletion.create(
+    model = model,
+    messages = message_input['messages'],
+    temperature = 0, #Si está más cercano a 1, es posible que tenga alucinaciones.
+    n = 1#, #Número de respuestas
+    max_tokens = 100
+    )
+
+# Imprime la respuesta del modelo
+result = response['choices'][0]['message']['content'].strip()
+print(result)
